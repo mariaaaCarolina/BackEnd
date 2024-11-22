802aa7f10cb415fd241b7e33bf40dc15f6d8f412
Maria Carolina de Oliveira Barbosa <mariahcarol02@gmail.com>
2024-11-22 16:52:00 -0300

    id do delete

--- src/router.js
+++ src/router.js
@@ -84,7 +84,7 @@ router.get("/applications", applicationController.getAll);
 router.get("/applications/:id", applicationController.getApplicationById);
 router.post("/application", applicationController.createApplication);
 router.put("/application", applicationController.updateApplication);
-router.delete("/application", applicationController.deleteApplication);
+router.delete("/application/:id", applicationController.deleteApplication);
 router.get(
     "/applications/vacancy/:vacancyId",
     applicationController.getApplicationsByVacancyId
