3fb458c1c7fde9070076fb87765eb61805edc322
Maria Carolina de Oliveira Barbosa <mariahcarol02@gmail.com>
2025-03-23 22:51:00 -0300

    alterando o connection acertando o servidor do bd (fé)

--- src/connection.js
+++ src/connection.js
@@ -1,23 +1,28 @@
 const mysql = require("mysql2/promise");
+const fs = require("fs");
 require("dotenv").config();
 
 const connect = async () => {
+    const path = "./src/DigiCertGlobalRootG2.crt.pem"; //alterar o caminho conforme onde o certificado está
+
     try {
-        const conn = await mysql.createConnection({
+        const ca = fs.readFileSync(path, "utf8");
+
+        conn = mysql.createConnection({
             host: process.env.MYSQL_HOST,
             user: process.env.MYSQL_USER,
             password: process.env.MYSQL_PASSWORD,
             database: process.env.MYSQL_DB,
             port: process.env.MYSQLPORT,
             ssl: {
-                rejectUnauthorized: false, // Desativa a verificação SSL (para desenvolvimento)
+                ca: ca, // certificado SSL
             },
         });
 
-        console.log("✅ Conexão ao banco de dados bem-sucedida!");
+        console.log("Conexão ao banco de dados bem-sucedida!");
         return conn;
     } catch (err) {
-        console.error(`❌ Erro ao conectar ao banco de dados: ${err.message}`);
+        console.error(`Erro ao conectar ao banco de dados: ${err.message}`);
     }
 };
 
